9b7c239001b1630e90d23a267515545ed15e2782
devil199301 <devil199301@gmail.com>
2018-07-25 17:45:17 +0800

    調整行為

--- node.js
+++ node.js
@@ -1,55 +1,80 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false
+    });
     const page = await browser.newPage();
+
+    /**
+     * 設定寬高
+     */
     const viewport = {
         width: 1300,
-        height: 900,
+        height: 800,
         // isMobile: true,
         // hasTouch: true,
     }
-    page.on('error', err => {
-        console.log('error happen at the page: ', err);
-    });
-    page.on('pageerror', pageerr => {
-        console.log('pageerror occurred: ', pageerr);
-    });
+    // page.on('error', err => {
+    //     console.log('error happen at the page: ', err);
+    // });
+    // page.on('pageerror', pageerr => {
+    //     console.log('pageerror occurred: ', pageerr);
+    // });
 
+    /**
+     * 設定網址
+     */
     const web = [{
-            "Id": "BW005-02.Portal",
-            "Name": "Run - 太阳城(原老虎机)  简易登入页面",
-            "PortalUrl2": "http://www.lhj8855.com/",
-            "SiteExist": true
-        },
-        {
-            "Id": "BW006-01.Portal",
-            "Name": "Run2 MG老虎机",
-            "PortalUrl2": "http://www.smart17.com/",
-            "SiteExist": true
-        }
-    ];
+            "Id": "",
+            "Name": "",
+            "PortalUrl2": "",
+            "SiteExist": 
+        }];
 
     for (var i = 0; i < web.length; i++) {
         try {
             await page.goto(web[i]['PortalUrl2']);
-            //   await page.setViewport(viewport);
-            await page.waitFor(1000);
-            await page.screenshot({
-                path: `${web[i]['Id']}.png`,
-                fullPage: true
-            });
-            console.log(`${web[i]['Id']}:'success'`)
+            console.log(`${web[i]['Id']} : 開始`);
+            await page.setViewport(viewport);
+            await page.waitFor(3000);
+
+            // 關閉iframe
+            try {
+                await page.click('.site-announcement span.closed');
+                console.log(`關閉iframe`);
+            } catch {};
+
+            // 關閉popup-news
+            try {
+                await page.click('span[ng-click="closeMarquee()"]');
+                console.log(`關閉popup-news`);
+            } catch {};
+
+            // 關閉popup-dialog
+            try {
+                await page.click('button.ui-dialog-titlebar-close');
+                console.log(`關閉popup-dialog`);
+            } catch {};
+
+            console.log(`5秒檢查`);
+            await page.waitFor(5000);
+
+            await page.click('a[href="/Lobby/Live"]');
+            console.log(`10秒檢查`);
+            await page.waitFor(10000);
+
+            // await page.screenshot({
+            //     path: `${web[i]['Id']}.png`,
+            //     fullPage: true // 全畫面截圖
+            // });
+            console.log(`${web[i]['Id']} : 結束`);
+            console.log(`下一站`);
         } catch (err) {
-            console.log(`Error loading page:${web[i]['Id']}`, err);
+            console.log(`Error loading : ${web[i]['Id']}`);
         }
     }
 
     await browser.close();
-    // await page.goto('http://www.00220040.com/');
-    // await page.click('#nav>ul>li.prmotion >a ')
-    // await page.type('input#login_account', 'gpkcs')
-    // await page.screenshot({
-    //     path: `tefst.png`
-    // });
+    console.log(`---------End---------`);
 })();
